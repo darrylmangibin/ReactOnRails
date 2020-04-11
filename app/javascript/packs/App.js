9c943a2807f10c2229554eab46522021b689ae30
Darryl Mangibin <darrylmangibin@gmail.com>
2020-04-11 12:12:03 +0800

    Add material ui

--- app/javascript/packs/App.js
+++ app/javascript/packs/App.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <div>App</div>
   )
 }
 
-export default App;
+export default App
--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 import App from "./App";
 
 document.addEventListener("DOMContentLoaded", () => {
 	ReactDOM.render(
-		<App />,
+		<CssBaseline>
+			<App />
+		</CssBaseline>,
 		document.body.appendChild(document.createElement("div"))
 	);
 });
